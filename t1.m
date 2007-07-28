bff49f33d07bdbac25a8ae476315f746f3cfad58
jorgefranconunes <jorgefranconunes@gmail.com>
2007-07-28 16:37:42 +0000

    Imported math tree.
    git-svn-id: svn+ssh://ssh.pdmfc.com/home/jfn/svn/math/trunk@1 05b82bb5-6cdd-4fc2-b0a3-f98a150675a1

---
+++ t1.m
@@ -0,0 +1,27 @@
+NPOINTS = 10000000;
+
+
+
+
+
+
+
+function result = getCoinTosses(nPoints, weight)
+
+  result = -1 + 2*(rand(1, nPoints) < weight);
+
+endfunction
+
+
+
+
+
+
+a = [ 1 -1];
+b = [ 0  1];
+
+x = getCoinTosses(NPOINTS, 0.5);
+y = filter(b, a, x);
+
+#plot(y);
+hist(y, 100);
